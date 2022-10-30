49924fe869ca8ec836c3914c0802f862425d94c7
bl0ckp1n9 <blockp1n9@gmail.com>
2022-10-30 02:02:42 +0000

    baejoon/1000

---
+++ 1000/1000.cpp
@@ -0,0 +1,14 @@
+/*
+두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+
+
+첫째 줄에 A와 B가 주어진다. (0 < A, B < 10).
+*/
+
+#include <iostream>
+using namespace std;
+int main() {
+  int A,B;
+  cin >> A >> B;
+  cout << A+B;
+}
